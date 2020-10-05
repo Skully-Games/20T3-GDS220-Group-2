025846e1ffdf1c7d4b9191934413fa1017e15070
Jesse <jesseanderton@hotmail.com>
2020-10-05 22:51:20 +1100

    Basic Resource Gathering

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,161 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public int wood = 0;
+    public int stone = 0;
+    public int food = 0;
+    public float water = 0;
+
+    public TextMeshProUGUI woodText;
+    public TextMeshProUGUI stoneText;
+    public TextMeshProUGUI foodText;
+    public TextMeshProUGUI waterText;
+    public TextMeshProUGUI pickUpMessage;
+
+    public GameObject Interactable;
+    public Camera camera;
+
+    private ResorcePickup itemBeingPickedUp;
+
+    void Update()
+    {
+        #region UI Stuff
+        woodText.text = wood.ToString();
+        stoneText.text = stone.ToString();
+        foodText.text = food.ToString();
+        waterText.text = water.ToString();
+
+        if (wood > 999)
+        {
+            wood = 999;
+        }
+
+        if (stone > 999)
+        {
+            stone = 999;
+        }
+
+        if (food > 999)
+        {
+            food = 999;
+        }
+
+        if (water > 999)
+        {
+            water = 999;
+        }
+        #endregion
+
+        SelectItemBeingPickedUpFromRay();
+
+        if (HasItemTargetted())
+        {
+            pickUpMessage.gameObject.SetActive(true);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (itemBeingPickedUp.tag == "Wood")
+                {
+                    wood++;
+                    Destroy(itemBeingPickedUp.gameObject);
+                    itemBeingPickedUp = null;
+                }
+                else if (itemBeingPickedUp.tag == "Stone")
+                {
+                    stone++;
+                    Destroy(itemBeingPickedUp.gameObject);
+                    itemBeingPickedUp = null;
+                }
+                else if (itemBeingPickedUp.tag == "Food")
+                {
+                    food++;
+                    Destroy(itemBeingPickedUp.gameObject);
+                    itemBeingPickedUp = null;
+                }
+                else if (itemBeingPickedUp.tag == "Water")
+                {
+                    water++;
+                    Destroy(itemBeingPickedUp.gameObject);
+                    itemBeingPickedUp = null;
+                }
+            }
+        }
+        else
+        {
+            pickUpMessage.gameObject.SetActive(false);
+        }
+       
+
+    }
+
+    private bool HasItemTargetted()
+    {
+        return itemBeingPickedUp != null;
+    }
+
+    private void SelectItemBeingPickedUpFromRay()
+    {
+        Ray ray = camera.ViewportPointToRay(Vector3.one / 2f);
+        Debug.DrawRay(ray.origin, ray.direction * 4f, Color.red);
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo, 4f))
+        {
+            var hitItem = hitInfo.collider.GetComponent<ResorcePickup>();
+
+            if(hitItem == null)
+            {
+                itemBeingPickedUp = null;
+            }
+            else if (hitItem != null && hitItem != itemBeingPickedUp)
+            {
+                itemBeingPickedUp = hitItem;
+                pickUpMessage.text = "Pick Up: " + itemBeingPickedUp.gameObject.name;
+            }
+            
+        }
+        else
+        {
+            itemBeingPickedUp = null;
+        }
+    }
+
+    #region Collision-based Collection
+    /*
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Wood")
+        {
+            Debug.Log("Player has collided with Wood");
+            wood ++;
+            Destroy(hit.gameObject);
+        }
+        else if (hit.gameObject.tag == "Stone")
+        {
+            Debug.Log("Player has collided with Stone");
+            stone++;
+            Destroy(hit.gameObject);
+        }
+        else if (hit.gameObject.tag == "Food")
+        {
+            Debug.Log("Player has collided with Food");
+            food++;
+            Destroy(hit.gameObject);
+        }
+        else if (hit.gameObject.tag == "Water")
+        {
+            Debug.Log("Player has collided with Water");
+            water++;
+            Destroy(hit.gameObject);
+        }
+    }
+    */
+    #endregion
+}
