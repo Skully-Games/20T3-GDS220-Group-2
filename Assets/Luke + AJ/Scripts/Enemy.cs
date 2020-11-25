bfac4be3c1f51b33b3047f01db02f283d89ec8c1
Luke <1019817@student.sae.edu.au>
2020-11-25 15:51:51 +1100

    Enemies patrol and chase states

---
+++ Assets/Luke + AJ/Scripts/Enemy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace LukeAJ
+{
+    public class Enemy : MonoBehaviour
+    {
+        [Header("Enemy references")]
+        
+        //May want to change this to reference the player script instead but just hacked it for now
+        public Transform player;
+        
+        public float speed;
+        public GameObject crystal;
+
+        //Patrol variables
+        [Header("Patrolling stuff")]
+        public Transform patrolPoint;
+        private float waitTime;
+        public float startWaitTime;
+        [Header("Crystal min and max Z and X positions (this will be the enemy's radius protecting the crystal)")]
+        public float minX;
+        public float maxX;
+        public float minZ;
+        public float maxZ;
+        
+        //Chase variables
+        [Header("Chase stuff")]
+        public float stoppingDistance;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //patrol
+            waitTime = startWaitTime;
+            patrolPoint.position = new Vector3(Random.Range(minX,maxX),crystal.transform.position.y, Random.Range(minZ, maxZ));
+        }
+
+        public void Patrol()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoint.position, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, patrolPoint.position) < 0.2f)
+            {
+                if (waitTime <= 0)
+                {
+                    patrolPoint.position = new Vector3(Random.Range(minX,maxX),crystal.transform.position.y, Random.Range(minZ, maxZ));
+                    waitTime = startWaitTime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+        }
+
+        public void Chase()
+        {
+            if (Vector3.Distance(transform.position, player.position) > stoppingDistance )
+            {
+                transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Luke + AJ/Scripts/EnemyStates.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LukeAJ
+{
+    public class EnemyStates : MonoBehaviour
+    {
+        //References
+        public Enemy enemy;
+        public Transform player;
+        public float playerSpottedDistance;
+        public float enemyLostSight;
+        
+        //State stuff 
+        public DelegateState currentState;
+        
+        public DelegateState patrol = new DelegateState();
+        public DelegateState chase = new DelegateState();
+        public DelegateState retreat = new DelegateState();
+
+        //These will help point to a function
+        [Serializable]
+        public class DelegateState
+        {
+            public Action Enter;
+            public Action Exit;
+            public Action Update;
+            public bool active;
+        }
+
+        //Change state manager
+        public void ChangeState(DelegateState newState)
+        {
+            if (currentState != null)
+            {
+                currentState.active = false;
+                currentState.Exit?.Invoke();
+            }
+
+            if (newState != null)
+            {
+                newState.active = true;
+                newState.Enter?.Invoke();
+                currentState = newState;
+            }
+        }
+
+        public void UpdateCurrentState()
+        {
+            currentState?.Update?.Invoke();
+        }
+
+        private void Start()
+        {
+            //doesn't run the functions just remembers it
+            patrol.Update = OnPatrolUpdate;
+            
+            chase.Update = OnChaseUpdate;
+
+            retreat.Enter = OnRetreatEnter;
+            retreat.Update = OnRetreatUpdate;
+            retreat.Exit = OnRetreatExit;
+
+            ChangeState(patrol);
+        }
+
+        //the different state functions
+        private void OnRetreatExit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnRetreatUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnRetreatEnter()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnChaseUpdate()
+        {
+            enemy.Chase();
+
+            if (Vector3.Distance(player.position, gameObject.transform.position) >= enemyLostSight)
+            {
+                ChangeState(patrol);
+            }
+        }
+
+        private void OnPatrolUpdate()
+        {
+            enemy.Patrol();
+            
+            if (Vector3.Distance(player.position,gameObject.transform.position) <= playerSpottedDistance)
+            {
+                ChangeState(chase);
+            }
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            UpdateCurrentState();
+        }
+            
+    }
+}
