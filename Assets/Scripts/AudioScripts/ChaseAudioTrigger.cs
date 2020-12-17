84c57647224c1c8ebb2fdd3ef5fdc2c5460a2329
Noel A <1015895@student.sae.edu.au>
2020-12-17 18:16:41 +1100

    Chase Music & Screech
    chase music and screech implemented

---
+++ Assets/Scripts/AudioScripts/ChaseAudioTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseAudioTrigger : MonoBehaviour
+{
+
+    [Header("Chase Music Controlls")]
+    public AudioSource chaseSource;
+    [SerializeField, Range(0, 1)] private float chaseVolume;
+
+    public float lookRadius = 10f;
+
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = ChaseAudioManager.instance.player.transform;
+
+        chaseVolume = chaseSource.GetComponent<AudioSource>().volume;
+        chaseVolume = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            chaseSource.GetComponent<AudioSource>().volume = chaseVolume = 1;
+            SoundManager.PlaySFX("Mscreech");
+        }
+        else if (distance>=lookRadius)
+        {
+            chaseSource.GetComponent<AudioSource>().volume = chaseVolume = 0;
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ Assets/Scripts/AudioScripts/CrystalSreech.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSreech : MonoBehaviour
+{
+    public float lookRadius = 3f;
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = ChaseAudioManager.instance.player.transform;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            SoundManager.PlaySFX("Mscreech");
+        }
+       
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
