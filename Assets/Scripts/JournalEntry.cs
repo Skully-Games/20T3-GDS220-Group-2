477d89548e560161816f64f157f825b73ce14be7
Anany Agarwal <1018790@student.sae.edu.au>
2020-12-17 14:13:24 +0530

    Journal Entry completed

--- Assets/Scripts/JournalEntry.cs
+++ Assets/Scripts/JournalEntry.cs
@@ -6,21 +6,16 @@ public class JournalEntry : MonoBehaviour
 {
 
     public GameObject journalEntry;
-    public GameObject statusBars;
-    public GameObject crosshair;
-    public GameObject pickupMessage;
     // Start is called before the first frame update
     void Start()
     {
         JournalOpen();
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void JournalClose()
     {
         journalEntry.SetActive(false);
-        statusBars.SetActive(true);
-        crosshair.SetActive(true);
-        pickupMessage.SetActive(true);
         Time.timeScale = 1f;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -28,9 +23,6 @@ public class JournalEntry : MonoBehaviour
     public void JournalOpen()
     {
         journalEntry.SetActive(true);
-        statusBars.SetActive(false);
-        crosshair.SetActive(false);
-        pickupMessage.SetActive(false);
         Time.timeScale = 0f;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
